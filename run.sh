427b71626c5f4264a0802ca940f8bb8c1dd93e4a
Daniel Beneyto <danfaizer@gmail.com>
2022-12-16 15:49:07 +0100

    Apply PR review suggestions

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@
 
 export PORT=${PORT:-8080}
 export PATH_STYLE=${PATH_STYLE:-false}
+export RANDOMIZE_CRON_MINUTE_INTERVAL=${RANDOMIZE_CRON_MINUTE_INTERVAL:-0}
 
 # Apply env variables
 cat config.toml | envsubst > run.toml
