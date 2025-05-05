e2a79f3768eddfd686c3ca1bc366e57a452565a8
Jesús Carpintero <jesus.carpintero@gmail.com>
2025-05-05 15:23:05 +0200

    Get home dir with os method (#176)

--- cmd/vulcan-crontinuous/commands/root.go
+++ cmd/vulcan-crontinuous/commands/root.go
@@ -18,7 +18,6 @@ import (
 	"github.com/julienschmidt/httprouter"
 	"github.com/sirupsen/logrus"
 
-	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
@@ -63,7 +62,7 @@ func initConfig() {
 		viper.SetConfigFile(cfgFile)
 	} else {
 		// Find home directory.
-		home, err := homedir.Dir()
+		home, err := os.UserHomeDir()
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
